cfcd43195e02cd9b7fd455b9e1c7b4c3e8500072
Stanislas Bernard <bernardstanislas@gmail.com>
2023-03-15 13:56:29 +0100

    :sparkles: Visit the HIR to build a program

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,55 @@
-use regex_syntax::hir::{visit, Hir, Visitor};
+use regex_syntax::hir::{visit, Hir, HirKind, Literal, Visitor};
 use regex_syntax::Parser;
 
-struct MyVisitor;
+#[derive(Debug)]
+enum Instruction {
+    Char(u8),
+    Match,
+}
 
-impl Visitor for MyVisitor {
+struct ProgramFactory {
+    instructions: Vec<Instruction>,
+}
+
+impl Default for ProgramFactory {
+    fn default() -> Self {
+        Self {
+            instructions: Vec::new(),
+        }
+    }
+}
+
+impl Visitor for ProgramFactory {
     type Err = ();
-    type Output = ();
+    type Output = Vec<Instruction>;
 
-    fn visit_pre(&mut self, _hir: &Hir) -> Result<(), Self::Err> {
-        println!("visit_pre");
-        println!("{:?}", _hir);
-        Ok(())
-    }
-
-    fn start(&mut self) {
-        println!("start");
-    }
-
-    fn visit_alternation_in(&mut self) -> Result<(), Self::Err> {
-        println!("visit_alternation_in");
-        Ok(())
-    }
-
-    fn visit_post(&mut self, _hir: &Hir) -> Result<(), Self::Err> {
-        println!("visit_post");
-        println!("{:?}", _hir);
+    fn visit_pre(&mut self, hir: &Hir) -> Result<(), Self::Err> {
+        match hir.kind() {
+            HirKind::Concat(_) => {}
+            HirKind::Literal(literal) => match literal {
+                Literal::Unicode(c) => {
+                    self.instructions.push(Instruction::Char(*c as u8));
+                }
+                Literal::Byte(b) => {
+                    self.instructions.push(Instruction::Char(*b));
+                }
+            },
+            HirKind::Empty => {
+                self.instructions.push(Instruction::Match);
+            }
+            _ => todo!(),
+        }
         Ok(())
     }
 
     fn finish(self) -> Result<Self::Output, Self::Err> {
-        println!("finish");
-        Ok(())
+        Ok(self.instructions)
     }
 }
 
 fn main() {
-    let hir = Parser::new().parse(r"[^abcf]+").unwrap();
-    let result = visit(&hir, MyVisitor {});
+    let hir = Parser::new().parse(r"abc").unwrap();
+    println!("{:?}", hir);
+    let result = visit(&hir, ProgramFactory::default());
     println!("{:?}", result);
 }
