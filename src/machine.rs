0643bfd52991ffa695f5d03b38039fd2152e2fe8
Iris <iris.devillars@gmail.com>
2023-03-23 07:26:22 -0400

    feat: update regex engine & add tests

--- src/machine.rs
+++ src/machine.rs
@@ -1,12 +1,31 @@
-use regex_syntax::hir::{Hir, HirKind, Literal, Visitor};
+use regex_syntax::hir::{
+    Anchor, Class, ClassUnicodeRange, Hir, HirKind, Literal, RepetitionKind, RepetitionRange,
+    Visitor,
+};
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum Instruction {
     Char(u8),
-    Match,
+    Match,            // Anchor end
+    Start,            // Anchor start
+    Repetition(u8),   // 0 to infinite repetition of a character
+    OptionalChar(u8), // in case of bounded repetitions or ZeroOrOneRepetition
+    IntervalChar(Vec<ClassUnicodeRange>),
 }
 
-pub type Program = Vec<Instruction>;
+#[derive(Default, Clone, Debug)]
+struct Action {
+    next: usize,
+    offset: i32,
+}
+
+#[derive(Debug, Clone)]
+pub struct ProgramItem {
+    instruction: Instruction,
+    action: Action,
+}
+
+pub type Program = Vec<ProgramItem>;
 
 pub struct Machine {
     program_counter: usize,
@@ -24,21 +43,98 @@ impl Machine {
     }
 
     pub fn run(&mut self, input: String) -> bool {
+        let mut state = 0;
+        let mut exact_match = false;
+
         while self.program_counter < self.program.len() {
-            let instruction = self.program.get(self.program_counter).unwrap();
-            self.program_counter += 1;
-            match instruction {
+            if state == self.program.len() {
+                // End of program, return true
+                return true;
+            }
+
+            let current_item = self.program.get(self.program_counter).unwrap();
+
+            match current_item.instruction.clone() {
                 Instruction::Char(c) => {
                     if self.string_counter >= input.len() {
                         return false;
                     }
-                    let result = input.as_bytes()[self.string_counter] == *c;
-                    self.string_counter += 1;
+                    let result = input.as_bytes()[self.string_counter] == c;
                     if !result {
-                        return false;
+                        // Failed match, backtrack to previous state
+                        let prev_state = self.program_counter.saturating_sub(1);
+                        let prev_item = self.program[prev_state].clone();
+                        state = prev_item.action.next;
+                        self.string_counter =
+                            (self.string_counter as i32 + prev_item.action.offset) as usize;
+                        self.program_counter = prev_state;
+                        if exact_match {
+                            return false;
+                        }
+                    } else {
+                        // Successful match, advance to next state
+                        state = current_item.action.next;
+                        self.string_counter =
+                            (self.string_counter as i32 + current_item.action.offset) as usize;
+                        self.program_counter += 1;
                     }
                 }
-                Instruction::Match => {}
+                Instruction::Match => {
+                    // check qu'on en est à la fin de la string
+                    return self.string_counter == input.len();
+                }
+                Instruction::Start => {
+                    self.program_counter += 1;
+                    exact_match = true;
+                }
+                Instruction::Repetition(c) => {
+                    let result = input.as_bytes()[self.string_counter] == c;
+                    if result {
+                        self.string_counter =
+                            (self.string_counter as i32 + current_item.action.offset) as usize;
+                    } else {
+                        state = current_item.action.next;
+                        self.program_counter += 1;
+                    }
+                }
+                Instruction::OptionalChar(c) => {
+                    let result = input.as_bytes()[self.string_counter] == c;
+                    if result {
+                        // if it matches we will go next character of the string
+                        self.string_counter =
+                            (self.string_counter as i32 + current_item.action.offset) as usize;
+                    }
+                    // if it doesn't match then we stay at the same sc but fo on pc right to the next state and next instruction
+                    state = current_item.action.next;
+                    self.program_counter += 1;
+                }
+                Instruction::IntervalChar(ranges) => {
+                    let mut has_matched = false;
+                    let result = input.as_bytes()[self.string_counter];
+                    for range in ranges.iter() {
+                        if range.start() as u8 <= result && result <= range.end() as u8 {
+                            // we're in the right range, it matches
+                            self.string_counter =
+                                (self.string_counter as i32 + current_item.action.offset) as usize;
+                            state = current_item.action.next;
+                            self.program_counter += 1;
+                            has_matched = true;
+                            break;
+                        }
+                    }
+                    if !has_matched {
+                        // If we're there then we haven't match anything yet
+                        let prev_state = self.program_counter.saturating_sub(1);
+                        let prev_item = self.program[prev_state].clone();
+                        state = prev_item.action.next;
+                        self.string_counter =
+                            (self.string_counter as i32 + prev_item.action.offset) as usize;
+                        self.program_counter = prev_state;
+                        if exact_match {
+                            return false;
+                        }
+                    }
+                }
             }
         }
         true
@@ -47,35 +143,250 @@ impl Machine {
 
 pub struct ProgramFactory {
     program: Program,
+    is_repetition: bool,
 }
 
 impl Default for ProgramFactory {
     fn default() -> Self {
         Self {
             program: Vec::new(),
+            is_repetition: false,
         }
     }
 }
 
 impl Visitor for ProgramFactory {
     type Err = ();
-    type Output = Vec<Instruction>;
+    type Output = Vec<ProgramItem>;
+
+    fn visit_post(&mut self, hir: &Hir) -> Result<(), Self::Err> {
+        if let HirKind::Repetition(_) = hir.kind() {
+            self.is_repetition = false;
+        }
+        Ok(())
+    }
 
     fn visit_pre(&mut self, hir: &Hir) -> Result<(), Self::Err> {
+        let mut start = 0;
+
         match hir.kind() {
             HirKind::Concat(_) => {}
-            HirKind::Literal(literal) => match literal {
-                Literal::Unicode(c) => {
-                    self.program.push(Instruction::Char(*c as u8));
+            HirKind::Literal(literal) => {
+                if !self.is_repetition {
+                    match literal {
+                        Literal::Unicode(c) => {
+                            self.program.push(ProgramItem {
+                                instruction: Instruction::Char(*c as u8),
+                                action: Action {
+                                    next: self.program.len() + 1 + start,
+                                    offset: 1,
+                                },
+                            });
+                        }
+                        Literal::Byte(b) => {
+                            self.program.push(ProgramItem {
+                                instruction: Instruction::Char(*b),
+                                action: Action {
+                                    next: self.program.len() + 1 + start,
+                                    offset: 1,
+                                },
+                            });
+                        }
+                    }
                 }
-                Literal::Byte(b) => {
-                    self.program.push(Instruction::Char(*b));
-                }
-            },
-            HirKind::Empty => {
-                self.program.push(Instruction::Match);
             }
-            _ => todo!(),
+            HirKind::Empty => {
+                self.program.push(ProgramItem {
+                    instruction: Instruction::Match,
+                    action: Action {
+                        next: self.program.len() + 1 + start,
+                        offset: 1,
+                    },
+                });
+            }
+            HirKind::Anchor(anchor) => match anchor {
+                Anchor::StartText => {
+                    start = self.program.len();
+                    self.program.push(ProgramItem {
+                        instruction: Instruction::Start,
+                        action: Action {
+                            next: start + 1,
+                            offset: 0,
+                        },
+                    });
+                }
+                Anchor::EndText => {
+                    self.program.push(ProgramItem {
+                        instruction: Instruction::Match,
+                        action: Action {
+                            next: self.program.len() + 1,
+                            offset: 0,
+                        },
+                    });
+                }
+                Anchor::StartLine => todo!(),
+                Anchor::EndLine => todo!(),
+            },
+            HirKind::Alternation(_) => todo!(),
+            HirKind::Repetition(repetition) => {
+                self.is_repetition = true;
+                match repetition.kind.clone() {
+                    RepetitionKind::OneOrMore => {
+                        if let HirKind::Literal(literal) = repetition.hir.kind() {
+                            let (instruction, repetition) = match literal {
+                                Literal::Unicode(c) => (
+                                    Instruction::Char(*c as u8),
+                                    Instruction::Repetition(*c as u8),
+                                ),
+                                Literal::Byte(b) => {
+                                    (Instruction::Char(*b), Instruction::Repetition(*b))
+                                }
+                            };
+                            self.program.push(ProgramItem {
+                                instruction,
+                                action: Action {
+                                    next: self.program.len() + 1 + start,
+                                    offset: 1,
+                                },
+                            });
+                            self.program.push(ProgramItem {
+                                instruction: repetition,
+                                action: Action {
+                                    next: self.program.len() + 1 + start,
+                                    offset: 1,
+                                },
+                            });
+                        }
+                    }
+                    RepetitionKind::ZeroOrMore => {
+                        if let HirKind::Literal(literal) = repetition.hir.kind() {
+                            let instruction = match literal {
+                                Literal::Unicode(c) => Instruction::Repetition(*c as u8),
+                                Literal::Byte(b) => Instruction::Repetition(*b),
+                            };
+                            self.program.push(ProgramItem {
+                                instruction,
+                                action: Action {
+                                    next: self.program.len() + 1 + start,
+                                    offset: 1,
+                                },
+                            });
+                        }
+                    }
+                    RepetitionKind::ZeroOrOne => {
+                        if let HirKind::Literal(literal) = repetition.hir.kind() {
+                            let instruction = match literal {
+                                Literal::Unicode(c) => Instruction::OptionalChar(*c as u8),
+                                Literal::Byte(b) => Instruction::OptionalChar(*b),
+                            };
+                            self.program.push(ProgramItem {
+                                instruction,
+                                action: Action {
+                                    next: self.program.len() + 1 + start,
+                                    offset: 1,
+                                },
+                            });
+                        }
+                    }
+                    RepetitionKind::Range(range) => match range {
+                        RepetitionRange::Exactly(n) => {
+                            if let HirKind::Literal(literal) = repetition.hir.kind() {
+                                let instruction = match literal {
+                                    Literal::Unicode(c) => Instruction::Char(*c as u8),
+                                    Literal::Byte(b) => Instruction::Char(*b),
+                                };
+
+                                for _i in 0..n {
+                                    self.program.push(ProgramItem {
+                                        instruction: instruction.clone(),
+                                        action: Action {
+                                            next: self.program.len() + 1 + start,
+                                            offset: 1,
+                                        },
+                                    });
+                                }
+                            }
+                        }
+                        RepetitionRange::AtLeast(n) => {
+                            if let HirKind::Literal(literal) = repetition.hir.kind() {
+                                let (instruction, repetition) = match literal {
+                                    Literal::Unicode(c) => (
+                                        Instruction::Char(*c as u8),
+                                        Instruction::Repetition(*c as u8),
+                                    ),
+                                    Literal::Byte(b) => {
+                                        (Instruction::Char(*b), Instruction::Repetition(*b))
+                                    }
+                                };
+
+                                for _i in 0..n {
+                                    self.program.push(ProgramItem {
+                                        instruction: instruction.clone(),
+                                        action: Action {
+                                            next: self.program.len() + 1 + start,
+                                            offset: 1,
+                                        },
+                                    });
+                                }
+                                self.program.push(ProgramItem {
+                                    instruction: repetition,
+                                    action: Action {
+                                        next: self.program.len() + 1 + start,
+                                        offset: 1,
+                                    },
+                                });
+                            }
+                        }
+                        RepetitionRange::Bounded(m, n) => {
+                            if let HirKind::Literal(literal) = repetition.hir.kind() {
+                                let (instruction, optional_char) = match literal {
+                                    Literal::Unicode(c) => (
+                                        Instruction::Char(*c as u8),
+                                        Instruction::OptionalChar(*c as u8),
+                                    ),
+                                    Literal::Byte(b) => {
+                                        (Instruction::Char(*b), Instruction::OptionalChar(*b))
+                                    }
+                                };
+                                for _i in 0..m {
+                                    self.program.push(ProgramItem {
+                                        instruction: instruction.clone(),
+                                        action: Action {
+                                            next: self.program.len() + 1 + start,
+                                            offset: 1,
+                                        },
+                                    });
+                                }
+
+                                for _i in 0..(n - m) {
+                                    self.program.push(ProgramItem {
+                                        instruction: optional_char.clone(),
+                                        action: Action {
+                                            next: self.program.len() + 1 + start,
+                                            offset: 1,
+                                        },
+                                    });
+                                }
+                            }
+                        }
+                    },
+                }
+            }
+            HirKind::Class(class) => match class {
+                Class::Unicode(set) => {
+                    let range_chars = set.ranges().to_owned();
+                    self.program.push(ProgramItem {
+                        instruction: Instruction::IntervalChar(range_chars),
+                        action: Action {
+                            next: self.program.len() + 1 + start,
+                            offset: 1,
+                        },
+                    })
+                }
+                Class::Bytes(_) => todo!(),
+            },
+            HirKind::Group(_) => {}
+            HirKind::WordBoundary(_) => {}
         }
         Ok(())
     }
--- src/tests.rs
+++ src/tests.rs
@@ -8,4 +8,263 @@ fn simple_string() {
     let program = visit(&hir, ProgramFactory::default()).unwrap();
     let mut machine = Machine::new(program);
     assert!(machine.run("abc".to_string()));
+    assert!(machine.run("123abc".to_string()));
+    assert!(machine.run("abc123".to_string()));
+    assert!(machine.run("123abc456".to_string()));
+}
+
+#[test]
+fn simple_string_end_matching_should_succeed() {
+    let hir = Parser::new().parse(r"abc$").unwrap();
+    let program = visit(&hir, ProgramFactory::default()).unwrap();
+    let mut machine = Machine::new(program);
+    assert!(machine.run("123abc".to_string()));
+}
+
+#[test]
+fn simple_string_end_matching_should_fail() {
+    let hir = Parser::new().parse(r"abc$").unwrap();
+    let program = visit(&hir, ProgramFactory::default()).unwrap();
+    let mut machine = Machine::new(program);
+    assert!(!machine.run("123abc456".to_string()));
+}
+
+#[test]
+fn simple_string_start_matching_should_succeed() {
+    let hir = Parser::new().parse(r"^abc").unwrap();
+    let program = visit(&hir, ProgramFactory::default()).unwrap();
+    let mut machine = Machine::new(program);
+    assert!(machine.run("abc123".to_string()));
+}
+
+#[test]
+fn simple_string_start_matching_should_fail() {
+    let hir = Parser::new().parse(r"^abc").unwrap();
+    let program = visit(&hir, ProgramFactory::default()).unwrap();
+    let mut machine = Machine::new(program);
+    assert!(!machine.run("123abc".to_string()));
+}
+
+#[test]
+fn simple_string_exact_matching_should_succeed() {
+    let hir = Parser::new().parse(r"^abc$").unwrap();
+    let program = visit(&hir, ProgramFactory::default()).unwrap();
+    let mut machine = Machine::new(program);
+    assert!(machine.run("abc".to_string()));
+}
+
+#[test]
+fn simple_string_exact_matching_should_fail() {
+    let hir = Parser::new().parse(r"^abc$").unwrap();
+    let program = visit(&hir, ProgramFactory::default()).unwrap();
+    let mut machine = Machine::new(program);
+    assert!(!machine.run("aabc".to_string()));
+}
+
+#[test]
+fn simple_string_exact_matching_should_fail_2() {
+    let hir = Parser::new().parse(r"^abc$").unwrap();
+    let program = visit(&hir, ProgramFactory::default()).unwrap();
+    let mut machine = Machine::new(program);
+    assert!(!machine.run("abccc".to_string()));
+}
+
+#[test]
+fn simple_string_one_or_more_matching_should_succeed() {
+    let hir = Parser::new().parse(r"^ab+c$").unwrap();
+    let program = visit(&hir, ProgramFactory::default()).unwrap();
+    let mut machine = Machine::new(program);
+    assert!(machine.run("abbc".to_string()));
+}
+
+#[test]
+fn simple_string_one_or_more_matching_should_succeed_2() {
+    let hir = Parser::new().parse(r"^ab+c$").unwrap();
+    let program = visit(&hir, ProgramFactory::default()).unwrap();
+    let mut machine = Machine::new(program);
+    assert!(machine.run("abc".to_string()));
+}
+
+#[test]
+fn simple_string_one_or_more_matching_should_fail() {
+    let hir = Parser::new().parse(r"^ab+c$").unwrap();
+    let program = visit(&hir, ProgramFactory::default()).unwrap();
+    let mut machine = Machine::new(program);
+    assert!(!machine.run("ac".to_string()));
+}
+
+#[test]
+fn simple_string_zero_or_more_matching_should_succeed() {
+    let hir = Parser::new().parse(r"^ab*c$").unwrap();
+    let program = visit(&hir, ProgramFactory::default()).unwrap();
+    let mut machine = Machine::new(program);
+    assert!(machine.run("ac".to_string()));
+}
+
+#[test]
+fn simple_string_zero_or_more_matching_should_succeed_2() {
+    let hir = Parser::new().parse(r"^ab*c$").unwrap();
+    let program = visit(&hir, ProgramFactory::default()).unwrap();
+    let mut machine = Machine::new(program);
+    assert!(machine.run("abbbc".to_string()));
+}
+
+#[test]
+fn simple_string_optional_matching_should_succeed() {
+    let hir = Parser::new().parse(r"^ab?c$").unwrap();
+    let program = visit(&hir, ProgramFactory::default()).unwrap();
+    let mut machine = Machine::new(program);
+    assert!(machine.run("abc".to_string()));
+}
+
+#[test]
+fn simple_string_optional_matching_should_succeed_2() {
+    let hir = Parser::new().parse(r"^ab?c$").unwrap();
+    let program = visit(&hir, ProgramFactory::default()).unwrap();
+    let mut machine = Machine::new(program);
+    assert!(machine.run("ac".to_string()));
+}
+
+#[test]
+fn simple_string_optional_matching_should_fail() {
+    let hir = Parser::new().parse(r"^ab?c$").unwrap();
+    let program = visit(&hir, ProgramFactory::default()).unwrap();
+    let mut machine = Machine::new(program);
+    assert!(!machine.run("abbc".to_string()));
+}
+
+#[test]
+fn simple_string_numbered_matching_should_succeed() {
+    let hir = Parser::new().parse(r"^ab{2}c$").unwrap();
+    let program = visit(&hir, ProgramFactory::default()).unwrap();
+    let mut machine = Machine::new(program);
+    assert!(machine.run("abbc".to_string()));
+}
+
+#[test]
+fn simple_string_numbered_matching_should_fail() {
+    let hir = Parser::new().parse(r"^ab{2}c$").unwrap();
+    let program = visit(&hir, ProgramFactory::default()).unwrap();
+    let mut machine = Machine::new(program);
+    assert!(!machine.run("abbbc".to_string()));
+}
+
+#[test]
+fn simple_string_numbered_matching_should_fail_2() {
+    let hir = Parser::new().parse(r"^ab{2}c$").unwrap();
+    let program = visit(&hir, ProgramFactory::default()).unwrap();
+    let mut machine = Machine::new(program);
+    assert!(!machine.run("abc".to_string()));
+}
+
+#[test]
+fn simple_string_numbered_matching_should_succeed_2() {
+    let hir = Parser::new().parse(r"^ab{3,}c$").unwrap();
+    let program = visit(&hir, ProgramFactory::default()).unwrap();
+    let mut machine = Machine::new(program);
+    assert!(machine.run("abbbc".to_string()));
+}
+
+#[test]
+fn simple_string_numbered_matching_should_succeed_3() {
+    let hir = Parser::new().parse(r"^ab{3,}c$").unwrap();
+    let program = visit(&hir, ProgramFactory::default()).unwrap();
+    let mut machine = Machine::new(program);
+    assert!(machine.run("abbbbbbc".to_string()));
+}
+
+#[test]
+fn simple_string_numbered_matching_should_fail_3() {
+    let hir = Parser::new().parse(r"^ab{3,}c$").unwrap();
+    let program = visit(&hir, ProgramFactory::default()).unwrap();
+    let mut machine = Machine::new(program);
+    assert!(!machine.run("abbc".to_string()));
+}
+
+#[test]
+fn simple_string_numbered_matching_should_succeed_4() {
+    let hir = Parser::new().parse(r"^ab{2,4}c$").unwrap();
+    let program = visit(&hir, ProgramFactory::default()).unwrap();
+    let mut machine = Machine::new(program);
+    assert!(machine.run("abbbbc".to_string()));
+}
+
+#[test]
+fn simple_string_numbered_matching_should_fail_4() {
+    let hir = Parser::new().parse(r"^ab{2,4}c$").unwrap();
+    let program = visit(&hir, ProgramFactory::default()).unwrap();
+    let mut machine = Machine::new(program);
+    assert!(!machine.run("abc".to_string()));
+}
+
+#[test]
+fn simple_string_numbered_matching_should_fail_5() {
+    let hir = Parser::new().parse(r"^ab{2,4}c$").unwrap();
+    let program = visit(&hir, ProgramFactory::default()).unwrap();
+    let mut machine = Machine::new(program);
+    assert!(!machine.run("abbbbbc".to_string()));
+}
+
+#[test]
+fn escaping_special_characters_should_succeed() {
+    let hir = Parser::new().parse(r"^\.$").unwrap();
+    let program = visit(&hir, ProgramFactory::default()).unwrap();
+    let mut machine = Machine::new(program);
+    assert!(machine.run(".".to_string()));
+}
+
+#[test]
+fn escaping_special_characters_should_succeed_2() {
+    let hir = Parser::new().parse(r"^\*$").unwrap();
+    let program = visit(&hir, ProgramFactory::default()).unwrap();
+    let mut machine = Machine::new(program);
+    assert!(machine.run("*".to_string()));
+}
+
+#[test]
+fn character_range_matching_should_succeed() {
+    let hir = Parser::new().parse(r"^[abc]$").unwrap();
+    let program = visit(&hir, ProgramFactory::default()).unwrap();
+    let mut machine = Machine::new(program);
+    assert!(machine.run("a".to_string()));
+}
+
+#[test]
+fn character_range_matching_should_fail() {
+    let hir = Parser::new().parse(r"^[abc]$").unwrap();
+    let program = visit(&hir, ProgramFactory::default()).unwrap();
+    let mut machine = Machine::new(program);
+    assert!(!machine.run("d".to_string()));
+}
+
+#[test]
+fn character_range_not_matching_should_succeed() {
+    let hir = Parser::new().parse(r"^[^ade]$").unwrap();
+    let program = visit(&hir, ProgramFactory::default()).unwrap();
+    let mut machine = Machine::new(program);
+    assert!(machine.run("b".to_string()));
+}
+
+#[test]
+fn character_range_not_matching_should_fail() {
+    let hir = Parser::new().parse(r"^[^ade]$").unwrap();
+    let program = visit(&hir, ProgramFactory::default()).unwrap();
+    let mut machine = Machine::new(program);
+    assert!(!machine.run("a".to_string()));
+}
+
+#[test]
+fn any_character_matching_should_succeed() {
+    let hir = Parser::new().parse(r"^.$").unwrap();
+    let program = visit(&hir, ProgramFactory::default()).unwrap();
+    let mut machine = Machine::new(program);
+    assert!(machine.run("A".to_string()));
+}
+
+#[test]
+fn case_insensitive_argument_should_succeed() {
+    let hir = Parser::new().parse(r"(?i)^abc$").unwrap();
+    let program = visit(&hir, ProgramFactory::default()).unwrap();
+    let mut machine = Machine::new(program);
+    assert!(machine.run("ABC".to_string()));
 }
